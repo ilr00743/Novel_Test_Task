6dc2a587e9e594fc3798c8e6771ea22def148192
illiaR <ir@readable.games>
2025-02-16 21:50:33 +0200

    Updated prefabs of map, updated configs, added UI textures and art for novel

---
+++ Assets/Scripts/UI/Map/CloseMapButton.cs
@@ -0,0 +1,34 @@
+﻿using Maps;
+using Naninovel;
+using Naninovel.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Map
+{
+    public class CloseMapButton : CustomUI
+    {
+        [SerializeField] private Button _closeButton;
+        private IStateManager _stateManager;
+        private IUIManager _uiManager;
+        private IScriptPlayer _scriptPlayer;
+
+        protected override void Start()
+        {
+            base.Start();
+            _closeButton.onClick.AddListener(OnClose);
+            _stateManager = Engine.GetService<IStateManager>();
+            _uiManager = Engine.GetService<IUIManager>();
+            _scriptPlayer = Engine.GetService<IScriptPlayer>();
+        }
+        
+        private void OnClose()
+        {
+            Hide();
+            _stateManager.Configuration.EnableStateRollback = true;
+            _uiManager.GetUI<OpenMapButton>().Show();
+            _uiManager.GetUI<MapView>().Hide();
+            _scriptPlayer.Play(_scriptPlayer.PlayedIndex);
+        }
+    }
+}
---
+++ Assets/Scripts/UI/Map/OpenMapButton.cs
@@ -0,0 +1,34 @@
+﻿using Maps;
+using Naninovel;
+using Naninovel.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.Map
+{
+    public class OpenMapButton : CustomUI
+    {
+        [SerializeField] private Button _openButton;
+        private IStateManager _stateManager;
+        private IUIManager _uiManager;
+        private IScriptPlayer _scriptPlayer;
+
+        protected override void Start()
+        {
+            base.Start();
+            _openButton.onClick.AddListener(OnOpen);
+            _stateManager = Engine.GetService<IStateManager>();
+            _uiManager = Engine.GetService<IUIManager>();
+            _scriptPlayer = Engine.GetService<IScriptPlayer>();
+        }
+        
+        private void OnOpen()
+        {
+            Hide();
+            _stateManager.Configuration.EnableStateRollback = false;
+            _uiManager.GetUI<CloseMapButton>().Show();
+            _uiManager.GetUI<MapView>().Show();
+            _scriptPlayer.Stop();
+        }
+    }
+}
