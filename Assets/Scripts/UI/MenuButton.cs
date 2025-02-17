0c3490c08181fc2d2bc2ce9cea750d24d8d5978a
illiaR <ir@readable.games>
2025-02-17 08:49:58 +0200

    Edited pauseUI and titleUI, created button for opening pauseUI

---
+++ Assets/Scripts/UI/MenuButton.cs
@@ -0,0 +1,25 @@
+﻿using Naninovel;
+using Naninovel.UI;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI
+{
+    public class MenuButton : CustomUI
+    {
+        [SerializeField] private Button _button;
+        private IUIManager _uiManager;
+        
+        protected override void Start()
+        {
+            base.Start();
+            _button.onClick.AddListener(OpenMenu);
+            _uiManager = Engine.GetService<IUIManager>();
+        }
+
+        private void OpenMenu()
+        {
+            _uiManager.GetUI<IPauseUI>().Show();
+        }
+    }
+}
